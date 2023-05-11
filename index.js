5404cb584665d64bae86afd843c25215fe6527d5
New Panuwat <new_sbh@me.com>
2023-05-11 13:32:25 +0700

    Add cors into api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const { users, bio, expLists, blogsAll, portPhoto, avatarPhoto } = require("./data");
 const app = express();
+app.use(cors());
+
 const PORT = 4000;
 
 app.listen(PORT, () => {
